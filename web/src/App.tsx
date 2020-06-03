ac1e852668bd093d1f0ee84b5fa3504a0970ae51
Matheus Baron <matheusbaron10@gmail.com>
2020-06-03 14:46:25 -0300

    Frontend completed and consuming the API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './App.css'
+
+import Routes from './Routes'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <Routes />
+    </div>
   );
 }
 
